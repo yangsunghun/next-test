17c4486cf7e0f14c5f3619a8b2c96736f3f058fe
yangsunghun <163554102+yangsunghun@users.noreply.github.com>
2024-12-11 20:00:09 +0900

    url은 유지하면서 파일만 변경해주는 rewrite 함수

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/home", request.url));
+  return NextResponse.rewrite(new URL("/home", request.url));
 }
 
 export const config = {
