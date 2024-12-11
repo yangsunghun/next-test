077cb967be08b75a05283c36d4093a7898182c54
yangsunghun <163554102+yangsunghun@users.noreply.github.com>
2024-12-11 20:26:00 +0900

    auth-json 설치 하기

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,5 +7,9 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
+  //matcher: ["/about/:path", "/dashboard/:path"], // 원하는 곳에서 미들웨어 실행시키기
   matcher: "/", // 어떤 페이지에서 middleware 를 실행시킬지 결정
 };
+
+// 어떤 요청, 어떤 path 에 들어갈 때, 유저의 상태가 어떠한지 미리 검증을 해서 유저가 안전한 상태로
+// 들어갈 수 있도록 해줌.
